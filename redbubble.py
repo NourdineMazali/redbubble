50d4afea54a04f4a0aa403346a27dfb7f4c66e1a
Redbubble Applicant <me@example.com>
2020-02-16 18:37:59 +1030

    TDD - test the cart items calculation

---
+++ redbubble.py
@@ -0,0 +1,33 @@
+import json
+"""
+    Domain module defines the objects related with the business model.
+    PriceFinder, Cart, CartItem, Calculator.
+"""
+
+
+class CartItem:
+    """
+        CartItem Class - Represents a CartItem in the Redbubble Shopping
+        Cart.
+        """
+    pass
+
+class Cart:
+    """
+    Cart Class - Represents a Shopping Cart in the Redbubble.
+
+    """
+    pass
+
+class PriceFinder:
+    """
+    PriceFinder is Price Lookup class for CartItems.
+    """
+    pass
+
+class Calculator:
+    """
+    Calculator Class for CartItems in the RedBubble
+    Shopping Cart.
+    """
+    pass
--- tests.py
+++ tests.py
@@ -6,6 +6,30 @@ import os
 import unittest #For Automated Testing Purposes
 from helper import Helper
 from exceptions import InputNotValid #For the invalid schema Exception
+from redbubble import Cart, PriceFinder, Calculator #Price Calculator Class - Find Price
+
+# ---------------------------------------------------------------------------- #
+# Initialization of Testing Variables - Easily Extendible and Dynamic!
+# ---------------------------------------------------------------------------- #
+root = os.path.join(Helper.ROOT_DIR, 'test_files', 'carts')
+list_of_cart_jsons = [
+    os.path.join(root, 'cart-4560.json'), \
+    os.path.join(root, 'cart-9363.json'), \
+    os.path.join(root, 'cart-9500.json'), \
+    os.path.join(root, 'cart-11356.json'), \
+    os.path.join(root, 'cart-0.json'), \
+    os.path.join(root, 'cart-no-options-one-item.json'), \
+    os.path.join(root, 'cart-repeat-item-quantity.json')]
+
+list_of_price_jsons = [os.path.join(Helper.ROOT_DIR, 'test_files','base_prices','base-prices.json')]
+
+list_of_checkout_prices = [4560, 9363, 9500, 11356, 0, 19500, 9120]
+
+list_of_cart_item_count = [1, 2, 2, 1, 0, 1, 2]
+
+list_of_price_item_count = [10]
+
+list_of_mark_up_prices = [4560, 9120, 243, 4560, 4940, 11356 ,19500, 4560, 4560]
 
 class TestFilesValidation(unittest.TestCase):
     """Tests for `cli` command line module."""
@@ -33,6 +57,33 @@ class TestFilesValidation(unittest.TestCase):
                 f=os.path.join(Helper.ROOT_DIR, 'test_files', 'test_carts' , 'cart-4560.json'),
                 schema=os.path.join(Helper.ROOT_DIR, 'test_files', 'test_invalid_schemas', 'base-prices-invalid-json.json')
             )
+
+
+class TestPriceCalculator(unittest.TestCase):
+
+    def test_cart_prices_json(self):
+        print()
+        print("Following Tests tested with Base Price File: " + \
+        list_of_price_jsons[0] + ":")
+        print()
+        for i in range(0, len(list_of_cart_jsons)):
+            print("Testing Cart JSON File: " + list_of_cart_jsons[i] + \
+            " with Expected Cart Value of " + str(list_of_checkout_prices[i]))
+
+            #Testing Base - cli.py
+            cart_json = Helper.load_schema(list_of_cart_jsons[i], 'cart.schema.json')
+            price_json = Helper.load_schema(list_of_price_jsons[0], 'base-prices.schema.json')
+            rb_cart = Cart(cart_json)
+            price_finder = PriceFinder(price_json)
+            price_calculator = Calculator(rb_cart, price_finder)
+            actual_cart_value = price_calculator.get_total()
+            print("Actual Cart Value: " + \
+                  str(price_calculator.get_total()), end ='')
+
+            self.assertEqual(actual_cart_value, list_of_checkout_prices[i])
+            print(" - PASS")
+            print()
+        print("-----------------------------------------------------------------------")
 
 if __name__ == '__main__':
     unittest.main()
